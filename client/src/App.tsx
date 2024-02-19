ef0456ae07e06ef237bf561585ce3a1794dbd878
Masha Moreva <mashamiskova@yandex.ru>
2024-02-19 18:42:07 +0300

    add-client-folder

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { FetchPostListView } from "./components/PostListView/FetchPostListView";
+
+function App() {
+  return (
+    <div className="app">
+      <FetchPostListView />
+    </div>
+  );
+}
+
+export default App;
