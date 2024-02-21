3562c6e75b5893f92e7c0d232ffe45cb428f6123
Masha Moreva <mashamiskova@yandex.ru>
2024-02-21 16:09:26 +0300

    add-posts-list-query

--- client/src/components/PostListView/FetchPostListView.tsx
+++ client/src/components/PostListView/FetchPostListView.tsx
@@ -1,21 +1,30 @@
-import { usePostList } from "../../api/Post";
+import { useQuery } from "@tanstack/react-query";
+import { fetchPostList } from "../../api/Post";
 import { Loader } from "../Loader";
 import { PostListView } from "./PostListView";
+import { queryClient } from "../../api/queryClient";
 
 export const FetchPostListView = () => {
-  const { state, refetch } = usePostList();
+  const postListQuery = useQuery(
+    {
+      queryFn: () => fetchPostList(),
+      queryKey: ["posts"],
+    },
+    queryClient
+  );
 
-  switch (state.status) {
-    case "idle":
+  switch (postListQuery.status) {
     case "pending":
       return <Loader />;
     case "success":
-      return <PostListView postList={state.data} />;
+      return <PostListView postList={postListQuery.data.list} />;
     case "error":
       return (
         <div>
           <span>Произошла ошибка: </span>
-          <button onClick={refetch}>Повторить запрос</button>
+          <button onClick={() => postListQuery.refetch()}>
+            Повторить запрос
+          </button>
         </div>
       );
   }
